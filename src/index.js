c2cc3887a6df986bb2478c6c1a542755f10a8383
brunonic17 <bscistri@gmail.com>
2024-01-09 23:17:39 -0300

    funcionamineto en puerto

--- src/index.js
+++ src/index.js
@@ -5,23 +5,26 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config(); 
 
-import './database/database';
+// import './database/database';
 
-import { routerProducts } from './routes/productsRoutes'; 
+// import { routerProducts } from './routes/productsRoutes.js'; 
 
 const app = express();
 
-app.set('PORT', process.env.PORT || 5000);
+const APP_PORT=process.env.APP_PORT;
 
-app.use(morgan('dev'));
-app.use(cors());
-app.use(express.json());
+// app.use(morgan('dev'));
+// app.use(cors());
+// app.use(express.json());
 
-app.use(routerProducts);
+// app.use(routerProducts);
 
-app.listen(app.get('PORT'), () => {
+app.listen( APP_PORT, () => {
     console.log(`Servidor ejecutándose en puerto
-    ${app.get('PORT')}`);
+    ${APP_PORT}`)
+    app.get('/',async (req,res)=>{
+      res.status(200).send({status:"OK", data:"sistema iniciado"})
+    })
 });
 
 
