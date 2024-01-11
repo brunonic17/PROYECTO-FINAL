b77ef1c65a8df655c596121643c2890e9643566c
Pablo Morcos <pemorcos@gmail.com>
2024-01-11 20:14:05 -0300

    Primer Commit Carito

---
+++ src/models/model.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+mongoose.pluralize (null);
+const CollectionShoppings = "Shoppings";
+
+const SchemaShoppings = new mongoose.Schema ({ 
+    IdCarrito : {type:"String", required:true},
+    
+})
