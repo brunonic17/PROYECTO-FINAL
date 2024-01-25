eaab3e19a75d3a45f119b666bc3a5622e7a7d9f5
delprafede <delpradofederico0@gmail.com>
2024-01-24 22:50:48 -0300

    registrando usuarios exitosamente

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,16 +1,21 @@
 import { Router  } from "express";
 import { login, register } from "../controllers/auth.controllers.js";
 import { check } from "express-validator";
-import validateResult from "../middelwares/validate.express.js";
 import { validatorAuth } from "../middelwares/auth.validate.js";
 
 const router = Router()
 
+
+
+
+
+
 router.post("/register",validatorAuth , register);
+
+
 
 
 router.post("/login", login)
 
 export default router
 
-//[check("nameUser", "El name es requerido").notEmpty(), validateResult ]
--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRouters from "../routes/auth.routes.js";
-
+import "dotenv/config.js"
 
 export const app = express();
 
 app.use(express.json()); //para la aplicación de análisis sintáctico /json
-app.use(express.urlencoded({ extended: true })); //Esta es una función de middleware incorporada en Express. Analiza las solicitudes entrantes. con cargas útiles codificadas en URL y se basa en body-parser .
 app.use(
   cors({
     origin: "*",
@@ -17,6 +16,7 @@ app.use(
     optionsSuccessStatus: 204,
   })
 );
+app.use(express.urlencoded({ extended: true })); //Esta es una función de middleware incorporada en Express. Analiza las solicitudes entrantes. con cargas útiles codificadas en URL y se basa en body-parser .
 app.use(cookieParser());
 
 app.use("/api", authRouters);
