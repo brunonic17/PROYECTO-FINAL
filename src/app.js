d1d936622d737fa18bb6146bacbe22bcb48ab210
delprafede <delpradofederico0@gmail.com>
2024-01-25 18:44:07 -0300

    form en correccion

--- src/app.js
+++ src/app.js
@@ -9,12 +9,12 @@ export const app = express();
 
 app.use(express.json()); //para la aplicación de análisis sintáctico /json
 app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    optionsSuccessStatus: 204,
-  })
+  cors(
+    // origin: "*",
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    // credentials: true,
+    // optionsSuccessStatus: 204,
+  )
 );
 app.use(express.urlencoded({ extended: true })); //Esta es una función de middleware incorporada en Express. Analiza las solicitudes entrantes. con cargas útiles codificadas en URL y se basa en body-parser .
 app.use(cookieParser());
