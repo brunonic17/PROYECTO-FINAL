b4b07bf0be4b78ecb07210cd281a3f396470d7e7
delprafede <delpradofederico0@gmail.com>
2024-03-22 15:46:22 -0300

    reestableciendo contraseña

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,22 +1,22 @@
 import { Hash } from "../helpers/password.helpers.js";
 import Users from "../models/user.models.js";
 import { createAccesToken } from "../libs/jwt.js";
-import bcrypt from "bcrypt";
-import  jwt  from "jsonwebtoken";
+import bcrypt, { hash } from "bcrypt";
+import jwt from "jsonwebtoken";
 import { tokenSecret } from "../config.js";
-
 
 //registrarse
 export const register = async (req, res) => {
+  //errors handling
   try {
     const { nameUser, email, password } = req.body;
 
-    const userFound = await Users.findOne({email})
-    if(userFound)
-      return res.status(409).json({msg: 'El email ya esta en uso'});
-    
+    const userFound = await Users.findOne({ email });
+    if (userFound)
+      return res.status(400).json({ msg: "El email ya esta en uso" });
+
     const passwordHash = await Hash(password);
-    
+
     const newUser = new Users({
       nameUser,
       email,
@@ -24,21 +24,19 @@ export const register = async (req, res) => {
     });
     // Save user to the database
     const userSaved = await newUser.save();
-    
+
     const token = await createAccesToken({ id: userSaved._id });
     res.cookie("token", token, {
       sameSite: "none",
-      secure: true
+      secure: true,
     });
 
     res.status(200).json({
-      messaje: "registrado existosamnete",
-      data: {
-        id: userSaved._id,
-        nameUser: userSaved.nameUser,
-        email: userSaved.email,
-        rule: userSaved.rule
-      },
+      id: userSaved._id,
+      nameUser: userSaved.nameUser,
+      email: userSaved.email,
+      rule: userSaved.rule,
+      msg: "Usuario creado correctamente",
     });
   } catch (error) {
     console.log(error);
@@ -46,78 +44,105 @@ export const register = async (req, res) => {
   }
 };
 //Loguearse
- export const login = async (req, res) => {
-    try {
-      const { email, password } = req.body;
-  
-      const userFound = await Users.findOne({ email });
-  
-      if (!userFound)
-        return res.status(400).send({
-          message: "Usuario o contrasena erronea",
-        });
-      // const validPass = await Compare(password, userFound.password);
-      const isMatch = await bcrypt.compare(password, userFound.password)
-      if (!isMatch)
-        return res.status(400).send({ message: "Usuario o contrasena erronea" });
-      //token
-      const token = await createAccesToken({ id: userFound._id });
-      res.cookie("token", token, {
-        sameSite: "none",
-        secure: true
-      });
-  
-      //(dto)
-  
-      res
-        .status(200)
-        .json({ ok: true, logged: isMatch, user: userFound, token: token });
-    } catch (error) {
-      res.status(400).send
-      ({ message: error.message });
-    }
-  };
-//Cerrar Sesion
-  export const logout = (req, res) => {
-    try {
-      res.cookie("token", "", {
-        expires: new Date(0),
-      });
-      res.status(200).json({ message: "Se cerro sesion" });
-    } catch (error) {
-     res.send(message.error);
-    }
-  };
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
- export const profile = async (req, res) => {
-    const userfound = await Users.findById(req.user.id)
-    
-    if(!userfound) return res.status(400).send({messaje: "usuario no encontrado"})
-    
+    const userFound = await Users.findOne({ email });
+
+    if (!userFound)
+      return res.status(400).send({
+        message: "Usuario o contrasena erronea",
+      });
+    // const validPass = await Compare(password, userFound.password);
+    const isMatch = await bcrypt.compare(password, userFound.password);
+    if (!isMatch)
+      return res
+        .status(400)
+        .send({ message: "Usuario o contrasena erroneaaaaa" });
+    //token
+    const token = await createAccesToken({ id: userFound._id });
+    res.cookie("token", token, {
+      sameSite: "none",
+      secure: true,
+    });
+
+    //(dto)
+
     res.status(200).json({
-      id: userfound._id,
-      nameUser: userfound.nameUser,
-      email: userfound.email,
-    })
-}
-
-export const verifyToken = async (req, res) => {
- const { token } = req.cookies
-
- if (!token) return res.status(400).send({message: "No hay Token"});
-
-  jwt.verify(token, tokenSecret, async (err,user) => {
-    if (err) return res.status(400).send({message:"Token Invalido"});
-    const userFound = await Users.findById(user.id)
-    if(!userFound) return res.status(400).send({message:'Usuario No Encontrado'})
-
-    return res.status(200).json({
-      id:userFound._id,
+      id: userFound._id,
       nameUser: userFound.nameUser,
       email: userFound.email,
       rule: userFound.rule,
-      data: "Verificacion Exitosa"
-    })
-  })
+      msg: "Usuario logueado correctamente",
+    });
+  } catch (error) {
+    res.status(400).send({ message: error.message });
   }
+};
+//Cerrar Sesion
+export const logout = (req, res) => {
+  try {
+    res.cookie("token", "", {
+      expires: new Date(0),
+    });
+    res.status(200).json({ message: "Se cerro sesion" });
+  } catch (error) {
+    res.send(message.error);
+  }
+};
 
+export const profile = async (req, res) => {
+  const userfound = await Users.findById(req.user.id);
+
+  if (!userfound)
+    return res.status(400).send({ messaje: "usuario no encontrado" });
+
+  res.status(200).json({
+    id: userfound._id,
+    nameUser: userfound.nameUser,
+    email: userfound.email,
+  });
+};
+
+export const verifyToken = async (req, res) => {
+  const { token } = req.cookies;
+
+  if (!token) return res.status(400).send({ message: "No hay Token" });
+
+  jwt.verify(token, tokenSecret, async (err, user) => {
+    if (err) return res.status(400).send({ message: "Token Invalido" });
+    const userFound = await Users.findById(user.id);
+    if (!userFound)
+      return res.status(400).send({ message: "Usuario No Encontrado" });
+
+    return res.status(200).json({
+      id: userFound._id,
+      nameUser: userFound.nameUser,
+      email: userFound.email,
+      rule: userFound.rule,
+      data: "Verificacion Exitosa",
+    });
+  });
+};
+//Cambiar Contraseña
+export const updatePassword = async (req, res) => {
+  const { password } = req.body;
+  try {
+    const userFound = await Users.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!userFound)
+      return res.status(400).json({ msg: "usuario no encontrado" });
+
+    const passwordHash = await Hash(password);
+    userFound.password = passwordHash;
+    userFound.save();
+
+    res.status(200).json({
+       userFound
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,30 +1,31 @@
-import { Router  } from "express";
-import { login, logout, profile, register, verifyToken} from "../controllers/auth.controllers.js";
+import { Router } from "express";
+import {
+  login,
+  logout,
+  register,
+  updatePassword,
+  verifyToken,
+} from "../controllers/auth.controllers.js";
 // import { check } from "express-validator";
 import { validatorAuth } from "../middelwares/auth.validate.js";
 import { authRequired } from "../middelwares/validateToken.js";
 
+const router = Router();
 
-const router = Router()
+router.post("/register", validatorAuth, register);
 
+router.post("/login", login);
 
+router.put("/forgotPassword/:id", updatePassword);
 
+router.post("/logout", logout);
 
-
-
-router.post("/register",validatorAuth, register);
-
-
-router.post("/login", login)
-
-
-router.post("/logout", logout)
+router.get("/verify", verifyToken);
 
 
-router.get("/verify", verifyToken )
 
 
-router.get("/profile", authRequired, profile)
 
-export default router
+// router.get("/profile", authRequired, profile);
 
+export default router;
